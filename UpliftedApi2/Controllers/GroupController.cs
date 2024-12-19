8742302416b9ebcf23f5ee8a7749ca98a0cf12a8
Zachary Handel <zachary.handelzh@gmail.com>
2024-12-19 15:21:08 -0600

    removed default controller, new group controller

---
+++ UpliftedApi2/Controllers/GroupController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UpliftedApi2.Models;
+
+namespace UpliftedApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        private readonly UpliftedApiContext _context;
+        public GroupController(UpliftedApiContext context)
+        {
+            _context = context;
+        }
+
+        // Get api/Group
+        [HttpGet]
+        public ActionResult<IEnumerable<Group>> GetGroups()
+        {
+            return _context.Groups.ToList();
+        }
+
+        // GET: api.Group/1
+        [HttpGet("{id}")]
+        public ActionResult<Group> GetGroup(int id)
+        {
+            var group = _context.Groups.Find(id);
+            if(group == null)
+            {
+                return NotFound();
+            }
+            return group;
+        }
+
+        //POST: api/Group
+        [HttpPost]
+        public ActionResult<Group> CreateGroup(Group group)
+        {
+            if(group == null)
+            {
+                return BadRequest();
+            }
+            _context.Groups.Add(group);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetGroup), new { id = group.Id }, group);
+        }
+
+    }
+}
