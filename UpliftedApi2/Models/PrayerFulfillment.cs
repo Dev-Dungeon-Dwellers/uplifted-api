d3cbaa63998e9c54d2edfc1f88d769a46b569e78
Zachary Handel <zachary.handelzh@gmail.com>
2024-12-19 14:53:13 -0600

    new models and updated entity framework in db context

---
+++ UpliftedApi2/Models/PrayerFulfillment.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Update.Internal;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UpliftedApi2.Models
+{
+    public class PrayerFulfillment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int prayerRequestId { get; set; }
+
+        [Required]
+        public DateTime createdAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        [MaxLength(1000)]
+        public string bodyText { get; set; }
+
+        [ForeignKey(nameof(prayerRequestId))]
+        public PrayerRequest myPrayerReqest { get; set; }
+    }
+}
---
+++ UpliftedApi2/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+
+namespace UpliftedApi2.Models
+{
+    public class Role
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string RoleName { get; set; }
+    }
+}
