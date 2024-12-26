355de161c298340aa049464e5b80ba9be77b2137
Zachary Handel <zachary.handelzh@gmail.com>
2024-12-26 08:38:43 -0600

    updated user controller to be async

---
+++ UpliftedApi2/Models/DTOs/CreateUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace UpliftedApi2.Models.DTOs
+{
+    public class CreateUserDto
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public int phoneNumber { get; set; }
+        public string profilePicture { get; set; }
+    }
+}
