4debe0b2bd585b11afa74f93b8cf3fd3710d55ba
Zachary Handel <zachary.handelzh@gmail.com>
2024-12-26 09:47:45 -0600

    removed current policy. Will be done in later task

--- UpliftedApi2/Controllers/GroupController.cs
+++ UpliftedApi2/Controllers/GroupController.cs
@@ -23,7 +23,6 @@ namespace UpliftedApi2.Controllers
         /// <returns></returns>
         // Get api/Group
         [HttpGet]
-        [Authorize(Policy = "ReadGroups")]
         public ActionResult<IEnumerable<Group>> GetGroups()
         {
             return _context.Groups.ToList();
